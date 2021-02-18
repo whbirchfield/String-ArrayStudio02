f5cf4a299b16559e3c152aaecd403c69dd5de956
Will B <willhbjr@gmail.com>
2021-02-18 08:24:15 +0000

    completed e.

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 console.log(protoArray1.includes(','));
 console.log(protoArray1.includes(';'));
 console.log(protoArray1.includes(' '));
-console.log("\n"),
+console.log("\n");
 console.log(protoArray2.includes(','));
 console.log(protoArray2.includes(';'));
 console.log(protoArray2.includes(' '));
-console.log("\n"),
+console.log("\n");
 console.log(protoArray3.includes(','));
 console.log(protoArray3.includes(';'));
 console.log(protoArray3.includes(' '));
-console.log("\n"),
+console.log("\n");
 console.log(protoArray4.includes(','));
 console.log(protoArray4.includes(';'));
 console.log(protoArray4.includes(' '));
@@ -28,15 +28,31 @@ console.log(protoArray4.includes(' '));
 let protoArrayOne = [];
 protoArrayOne = protoArray1.split(',').reverse();
 protoArray1 = protoArrayOne.join(',');
+console.log(protoArrayOne)
 console.log(protoArray1);
-
+console.log("\n");
 
 //c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
-
+let protoArrayTwo = [];
+protoArrayTwo = protoArray2.split(';').sort();
+console.log(protoArrayTwo)
+protoArray2 = protoArrayTwo.join(',');
+console.log(protoArray2);
+console.log("\n");
 
 
 //d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
-
+let protoArrayThree = [];
+protoArrayThree = protoArray3.split(' ').sort();
+console.log(protoArrayThree);
+protoArray3 = protoArrayThree.join(' ');
+console.log(protoArray3);
+console.log("\n");
 
 
 //e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
+let protoArrayFour = [];
+protoArrayFour = protoArray4.split(', ').reverse();
+protoArray4 = protoArrayFour.join(',');
+console.log(protoArrayFour);
+console.log(protoArray4);
